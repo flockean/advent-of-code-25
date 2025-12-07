4974c01f286efcd6042af864725bc6feafa2000e
flockean <lucas.buchholz1@gmail.com>
2025-12-07 13:31:28 +0100

    feat: daytwo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 @main def hello(): Unit =
   println("Hello Advent of Code!")
-  dayone.main()
+  daytwo.main()
 
 object dayone{
   var dial: Int = 50
@@ -44,8 +44,77 @@ object dayone{
 }
 
 
+object daytwo{
+  var foundNumbers: List[Long] = List()
+  var sumOfInvalidIDs: Long = 0
+
+  def isInvalid(number: Long): Boolean = {
+    val numStr = number.toString
+    val length = numStr.length
+    
+    // even length check
+    if (length % 2 != 0) return false
+    
+    val half = length / 2
+    val firstHalf = numStr.substring(0, half)
+    val secondHalf = numStr.substring(half)
+    
+    // check identical
+    firstHalf == secondHalf
+  }
+
+  def isInvalidMorethanTwice(number: Long): Boolean = {
+    val numStr = number.toString
+    val length = numStr.length
+    
+    // even length check
+    for (patternLen <- 1 to length / 2) {
+      if (length % patternLen == 0) {
+        val pattern = numStr.substring(0, patternLen)
+        val timesRepeated = length / patternLen
+        
+        // at least twice repeated
+        if (timesRepeated >= 2) {
+          val reconstructed = pattern * timesRepeated
+          if (reconstructed == numStr) {
+            return true
+          }
+        }
+      }
+    }
+    false
+  }
+
+  def detectRange(range: String): Unit = {
+    val parts = range.split("-")
+    val start = parts(0).toLong
+    val end = parts(1).toLong
+
+    for (number <- start to end) {
+      if (isInvalidMorethanTwice(number)) {
+        foundNumbers = foundNumbers :+ number
+      }
+    }
+  }
+
+  def main(): Unit = {
+    for (line <- readDocument.lines) {
+      detectRange(line)
+    }
+    for (num <- foundNumbers) {
+      sumOfInvalidIDs += num
+    }
+    println(sumOfInvalidIDs)
+  }
+}
+
+
 object readDocument{
-  val lines = scala.io.Source.fromFile("input.txt").getLines.toList
+  // Read lines
+  // val lines = scala.io.Source.fromFile("input.txt").getLines.toList
+
+  // Read line comma separated
+  val lines = scala.io.Source.fromFile("input.txt").getLines().next().split(",").toList
 
 }
 
